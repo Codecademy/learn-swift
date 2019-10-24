9e8f375c11871edcbed03cba90da26030b5f9f16
Sonny Li <sonny@codecademy.com>
2019-10-24 17:01:03 -0400

    upload hello world module

---
+++ 1-hello-world/block-letters/initials.cpp
@@ -0,0 +1,18 @@
+// Sonny Li
+// Fun Fact: I played guitar in a band called Attica.
+
+#include <iostream>
+
+int main() {
+
+  std::cout << "  SSS   L     \n";
+  std::cout << " S   S  L     \n";
+  std::cout << " S      L     \n";
+  std::cout << "  SSS   L     \n";
+  std::cout << "     S  L     \n";
+  std::cout << " S   S  L     \n";
+  std::cout << "  SSS   LLLLL \n";
+
+  return 0;
+
+}
