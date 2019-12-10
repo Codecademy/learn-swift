cf20f647c782b6705b16b8edbf1619072baadfc4
Sonny Li <sonny@codecademy.com>
2019-12-10 17:53:01 -0500

    Update DogYears.swift

--- 2-variables/dog-years/DogYears.swift
+++ 2-variables/dog-years/DogYears.swift
@@ -2,11 +2,10 @@
 // Sonny Li
 
 var dogAge: Double = 2.5
-var earlyYears: Double = 0.0
+
+let earlyYears: Double = 21.0
 var laterYears: Double = 0.0
 var humanYears: Double = 0.0
-
-earlyYears = 21
 
 laterYears = (dogAge - 2) * 4
 
