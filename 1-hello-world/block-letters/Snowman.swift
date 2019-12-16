4a400174271ad20f5ecc8911be859d8c46e4e250
Sonny Li <sonny@codecademy.com>
2019-12-16 12:34:00 -0500

    ❄️

---
+++ 1-hello-world/block-letters/Snowman.swift
@@ -0,0 +1,21 @@
+// Fun Fact: I drink a looot of water.
+// Twitter: @anne_melody
+
+print(" *                *            * ")
+print("      *     *                    ")
+print("                      *          ")
+print("*         *                 *    ")
+print("     *           *     *         ")
+print("           HHHHHHH        *      ")
+print(" *         HHHHHHH               ")
+print("     *   HHHHHHHHHHH    *     *  ")
+print("         *   @ @   *             ")
+print("        *     ^     *            ")
+print("  *       *       *         *    ")
+print("         SSS * * S               ")
+print("         * SSSSSSSS *            ")
+print("       *      O   SS  *          ")
+print("      *       O    S   *         ")
+print("      *       O        *         ")
+print("       *              *          ")
+print("           *   *   *             ")
