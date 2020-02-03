74ce5353d9e3732f4bef6dcdff31cc09acce3532
Sonny Li <sonnynomnom@gmail.com>
2020-02-03 01:03:59 -0500

    update

---
+++ 5-arrays/palindrome/Palindrome.swift
@@ -0,0 +1,25 @@
+// Palindrome
+// Sonny Li
+
+var text = ["h", "e", "l", "l", "o"]
+
+var reversed = [String]()
+
+var counter = text.count - 1
+
+while(counter >= 0) {
+  
+  reversed.append(text[counter])
+  counter = counter - 1
+  
+}
+
+if (text == reversed) {
+  
+  print("We have a palindrome!")
+  
+} else {
+  
+  print("\(text) is not palindrome.")
+  
+}
