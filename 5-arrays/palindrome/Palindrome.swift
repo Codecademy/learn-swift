761f789094afeaf7dccd3575400ba7f5cdb387a1
Sonny Li <sonnynomnom@gmail.com>
2020-02-07 15:43:49 -0500

    Update Palindrome.swift

--- 5-arrays/palindrome/Palindrome.swift
+++ 5-arrays/palindrome/Palindrome.swift
@@ -7,19 +7,13 @@ var reversed = [String]()
 
 var counter = text.count - 1
 
-while(counter >= 0) {
-  
+while counter >= 0 {
   reversed.append(text[counter])
   counter = counter - 1
-  
 }
 
-if (text == reversed) {
-  
+if text == reversed {
   print("We have a palindrome!")
-  
 } else {
-  
   print("\(text) is not palindrome.")
-  
 }
