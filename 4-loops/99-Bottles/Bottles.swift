649bd2b59ce1e12526def50437f7b6bba184aa4c
adistasicc <59616028+adistasicc@users.noreply.github.com>
2020-02-10 16:34:55 -0500

    Update Bottles.swift

--- 4-loops/99-Bottles/Bottles.swift
+++ 4-loops/99-Bottles/Bottles.swift
@@ -1,10 +1,10 @@
 // Bottles (Swift) - Prints the full lyrics to "99 Bottles of Milk" song
 // Alex DiStasi
 
-//numBottles represents the number of bottles left on the wall
+// numBottles represents the number of bottles left on the wall
 var numBottles: Int = 99
 
-//While there are more than 0 bottles on the wall, print the song lyrics
+// While there are more than 0 bottles on the wall, print the song lyrics
 while numBottles > 0 {
   
   print ("\(numBottles) bottles of milk on the wall, \(numBottles) bottles of milk!")
@@ -17,6 +17,17 @@ while numBottles > 0 {
 
 print ("\(numBottles) bottles of milk on the wall, \(numBottles) bottles of milk!\nGo to the store and buy some more,\n99 bottles of milk on the wall!")
 
+/*
+// Solution to optional challenge 1
 
+for numBottles in stride(from: 99, to: 0, by: -1){
+  print ("\(numBottles) bottles of milk on the wall, \(numBottles) bottles of milk!")
+  print ("You take one down and pass it around...")
+  print ("\(numBottles-1) bottles of milk on the wall!")
+}
+
+print ("0 bottles of milk on the wall, 0 bottles of milk!\nGo to the store and buy some more,\n99 bottles of milk on the wall!")
+
+*/
 
 
