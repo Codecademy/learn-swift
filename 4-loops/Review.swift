e1340a7419aee4d9122422f9a151c08c7336fa11
linkcoding <kenny.lin.91@gmail.com>
2020-03-02 16:53:33 -0500

    added comments to explain code

--- 4-loops/Review.swift
+++ 4-loops/Review.swift
@@ -2,6 +2,7 @@
 
 // First challenge:
 for num in 1...100 {
+  // If a number is fully divisible by 2, it's an even number
   if num % 2 == 0 {
     print("even")
   } else {
@@ -11,11 +12,15 @@ for num in 1...100 {
 
 
 // Second Challenge 
+// checkPrime can be changed to a positive number greater than 1
 var checkPrime = 17
+// Assume the number is prime until proven otherwise
 var isPrime = true
-for num in stride(from: checkPrime - 1, to: 1, by: -1) {
+for num in 2...checkPrime - 1 {  
+  // If checkPrime is fully divisible by the current number it's not a prime number
   if checkPrime % num == 0 {
     isPrime = false
+    break
   }
 }
 
@@ -32,7 +37,9 @@ print("There are \(counter) characters in that string!")
 
 
 // Fourth Challenge
+// use starTracker to store the # of *s 
 var starTracker = ""
+// stopNum controls when to stop the loop
 var stopNum = 4
 
 for _ in 1...stopNum {
