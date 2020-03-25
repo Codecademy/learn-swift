cb7adeae19bd891c07a722f5eb377968d7c3bfb4
linkcoding <kenny.lin.91@gmail.com>
2020-03-25 14:37:47 -0400

    added second half of project

--- 8-structures/Structured Exercise Regimen/ExerciseRegimen.swift
+++ 8-structures/Structured Exercise Regimen/ExerciseRegimen.swift
@@ -16,3 +16,25 @@ struct Exercise {
 
 var pushUp = Exercise(name: "Push Up", muscleGroups: ["Triceps", "Chest", "Shoulders"], reps: 10, sets: 3)
 
+struct Regimen {
+  var dayOfWeek: String 
+  var exercises: [Exercise]
+
+  init(dayOfWeek: String, exercises: [Exercise]) {
+    self.dayOfWeek = dayOfWeek
+    self.exercises = exercises
+  }
+
+  func exercisePlan() {
+    print("Today is \(self.dayOfWeek) and you're going to:")
+    for exercise in self.exercises {
+      print("Do \(exercise.sets) sets of \(exercise.reps) \(exercise.name)s")
+      print("That's a total of \(exercise.totalReps) \(exercise.name)s")
+    }
+  }
+}
+
+var mondayRegimen = Regimen(dayOfWeek: "Monday", exercises:[pushUp])
+
+mondayRegimen.exercisePlan()
+
