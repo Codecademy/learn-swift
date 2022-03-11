0d6249dd8ce7f74bc57af7a4301300a6ece88e7a
d-llirium <plimacarlos@gmail.com>
2022-03-11 17:03:54 -0300

    over write letter and add Initials swift files

--- 1-hello-world/Letter.swift
+++ 1-hello-world/Letter.swift
@@ -1,4 +1,1 @@
-print("Dear Self,")
-print("Build a dope mobile app.")
-print("Sonny")
-print("January 2020, New York")
+print("Dear Self, by the end of this course I want to be able to build an app by myself and solving problems without help. Patricia Carlos, jun 11th 2021")
---
+++ 1-hello-world/block-letters/InitialsPC.swift
@@ -0,0 +1,10 @@
+// Patricia Carlos
+//fun fact : - i'm a drag queen
+
+print("PPPP "+" "+" CCC")
+print("P   P"+" "+"C   C")
+print("P   P"+" "+"C")
+print("PPPP "+" "+"C")
+print("P    "+" "+"C")
+print("P    "+" "+"C   C")
+print("P    "+" "+" CCC")
